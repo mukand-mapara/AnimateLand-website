7dc16065b9eb0602cacedb4ba3dcfa30671223ab
Mukand Mapara <129943687+mukand-mapara@users.noreply.github.com>
2025-06-21 21:26:53 +0500

    updated code
    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Hero from "./components/Hero";
 import About from "./components/About";
-import Navbar from "./components/Navbar";
-import Features from "./components/Features";
-import Story from "./components/Story";
 import Contact from "./components/Contact";
+import Features from "./components/Features";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Story from "./components/Story";
 
 const App = () => {
   return (
